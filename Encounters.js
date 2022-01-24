fff1d46394698deedf3d1c9a621dddf511cae0c7
Alan Wightman <97927281+AlanWman@users.noreply.github.com>
2022-01-24 06:12:24 -0500

    Add comments. Clean up layout.

--- Encounters.js
+++ Encounters.js
@@ -1,35 +1,11 @@
-/*
-Creature types from 5e SRD rules - http://5e.d20srd.org/srd/monsters/intro.htm
-1. Aberrations: Slaad, Otyugh, Gibbering Mouther, Aboleth
-2. Celestials: Pegasus, Coutl, Angel, Empyrean, Unicorn
-3. Constructs: Golem, Homunculus, Animated Objects, Shield Guardian
-4. Dragons: Red, Blue, Green, Black, White, Gold, Silver, Bronze, Brass, Copper
-5. Elementals: Earth, Air, Fire, Water
-6. Fey: Lord, Hag, Dryad, Sprite/Pixie
-7. Fiends: Demon, Devil, Cambion, Night Hag.
-8. Giants:  Frost, Fire, Stone, Storm, Hill
-9. Humanoid:  goblinoids, orcs, gnolls, lizardfolk, kobolds
-10. Monstrosities: Winter Wolf, Bulette, Roc, Medusa
-11. Undead: Zombies, Wight, Vampire, Lich
+/*Calls Encounter.*/
+Encounter();
 
-Locations
-1. Settlement - City, town, village, outpost
-2. Dungeon
-3. Temple - Good, Evil, Pantheon
-4. Fortress 
-5. Tavern
-6. Forest Grove
+/*FUNCTIONS*/
+/*Roll singular die of side 'upperbound'*/
+const roller = (upperbound) => Math.floor(Math.random() * x + 1);
 
-Reaction Roll Table from Basic Fantasy Roleplay - https://www.basicfantasy.org/srd/#reaction_roll_table
-Adjusted Die Roll	Result
-2 or less	Immediate Attack
-3-7	        Unfavorable
-8-11	    Favorable
-12 or more	Very Favorably
-*/
-
-
-const roller = (x) => Math.floor(Math.random() * x + 1);
+/* Simulate roll of any homogenous set of dice*/
 const XdXRoller = (numOfDice, numOfSides) => {
     
     let result = 0;
@@ -41,7 +17,6 @@ const XdXRoller = (numOfDice, numOfSides) => {
 }
 
 /*Determine Encounter*/
-
 const Encounter = () => {
     const creature = creatures();
     const location = locations();
@@ -55,6 +30,8 @@ const Encounter = () => {
 /*Determine Creature*/
 const creatures = () => {
     let roll = roller(11);
+
+    /*Used in switch cases to determine array listing used*/
     let subRoll = undefined;
 
     /*Creature List Object*/
@@ -71,7 +48,6 @@ const creatures = () => {
         Monstrosities: ['Winter Wolf', 'Bulette', 'Roc', 'Medusa'],
         Undead: ['Zombies', 'Wight', 'Vampire', 'Lich']
     }
-
     switch (roll) {
         case 1:
             subRoll = roller(oCreatures.Aberrations.length - 1)
@@ -170,7 +146,7 @@ const reactions = () => {
         return 'Something went wrong'
     }
 }
-/*
+/* tests
 console.log(creatures());
 console.log(locations());
 console.log(reactions());
